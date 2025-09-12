daf65981d40c6b8c087bcf0ee4ad9d4577d65c69
Aman Jolly <42204610+amanjolly@users.noreply.github.com>
2025-09-12 09:03:59 +0530

    Stream API Questions Practice added

---
+++ LeetCode/src/main/java/com/aj/java/codingExercises/strings/Deloitte_Test.java
@@ -0,0 +1,18 @@
+import com.sun.xml.internal.messaging.saaj.packaging.mime.util.LineInputStream;
+
+import java.util.*;
+
+public class Test {
+    public static void main(String[] args) {
+        // ["Test","XYZ","Hello","Java","India"]
+        // Extract first Vauval from each string and create a new string
+
+        List<String> inputString = Arrays.asList("Test","XYZ","Hello","Java", "India");
+
+       // s="abcdpqrbdpar"
+        // output = b
+        String input="abcdpqrbdpar";
+        HashSet<String> visitedChar = new HashSet<String>();
+        input.
+    }
+}
