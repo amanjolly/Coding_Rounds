8a7096a131e0c35edc6363550e0a2f2bf6e89e71
Aman Jolly <42204610+amanjolly@users.noreply.github.com>
2025-09-15 13:14:15 +0530

    Stream API Questions Added

--- LeetCode/src/main/java/com/aj/java/codingExercises/strings/Deloitte_Test.java
+++ LeetCode/src/main/java/com/aj/java/codingExercises/strings/Deloitte_Test.java
@@ -1,18 +1,46 @@
-import com.sun.xml.internal.messaging.saaj.packaging.mime.util.LineInputStream;
+package com.aj.java.codingExercises.strings;
 
-import java.util.*;
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Optional;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
-public class Test {
+public class Deloitte_Test {
     public static void main(String[] args) {
         // ["Test","XYZ","Hello","Java","India"]
-        // Extract first Vauval from each string and create a new string
+        // Extract first Vowel from each string and create a new string
 
-        List<String> inputString = Arrays.asList("Test","XYZ","Hello","Java", "India");
+        List<String> inputStringList = Arrays.asList("Test", "XYZ", "Hello", "Java", "India");
 
-       // s="abcdpqrbdpar"
+        // Data -> stream of words -> stream of char -> filter vowels -> collect
+
+
+        // str ="abcemloq";
+        // Extract vowels and form a string
+        // Data -> stream of char -> filter vowels -> collect
+        String inputString ="abcemloq";
+       // inputString.chars().mapToObj(ch -> (char)ch).filter(ch -> ch == ;
+
+        Pattern pattern = Pattern.compile("[AEIOUaeiou]");
+        Matcher matcher = pattern.matcher(inputString);
+        System.out.println(matcher.group());
+
+        // s="abcdpqrbdpar"
         // output = b
-        String input="abcdpqrbdpar";
+
+        String input = "abcdpqrbdpar";
         HashSet<String> visitedChar = new HashSet<String>();
-        input.
+        //input.
+    }
+
+    private static Optional<Character> extractVowelFromWord(String word) {
+        for (char ch : word.toCharArray()) {
+            if ("AEIOUaeiou".indexOf(ch) != -1) {
+                return Optional.of(ch);
+            }
+        }
+        return Optional.empty();
     }
 }
--- README.md
+++ README.md
@@ -23,4 +23,28 @@ Java 8 Stream API Practice Questions | Interview Questions
 - [ ] Q16: Group /Pair anagrams from a list of Strings, For 1 word consider only one anagram?
     	  List<String> strings = {"pan","nap","cafe","face","meat", "tree","team"}
     		//Expected O/P [[pan,nap],[cafe,face],[meat,team], [tree]]
-- [ ] Q17: 
+- [ ] Q17: Write a stream program to multiply alternative numbers in an array ?
+          int[] intArray ={4,5,1,7,2,9,2}
+            //Expected O/P 4x1x2x2 =16
+- [ ] Q18: Write a program to multiply 1st and last element, 2nd and 2nd last element etc in an integer array.  
+          int[] intArray={4,5,1,7,9,2}  
+           //Expected O/P 8, 45, 7
+- [ ] Q19: Write a stream program to move all zero’s to the beginning of array?  
+           int[] arr = {5,0,1,0,8,0}  
+           //Expected O/P {0,0,0,5,1,8}
+- [ ] Q20: In a given array of integers, return true if it contains distinct values and false otherwise  
+           int[] intArray = {5,0,1,0,8,0}  
+            //Expected O/P false
+- [ ] Q21: Given the string[] array group the strings based on the middle character  
+           String[] strArray ={"ewe", "jji", "jhj", "kwk", "aha"}  
+            //Expected O/P {w=["ewe", "kwk"], h=["jhj", "aha"], j=["jji"]
+- [ ] Q22: Find the sum of all the elements in a list
+            I/P List<Integer> intList = Arrays.asList(1,2,3,4,5);
+            //Expected O/P 15
+- [ ] Q23: Sort a list of strings in alphabetical order
+- [ ] Q24: Convert a list of integers to a list of their squares  
+           List <integer>integerList = List.of(1, 2, 3, 4, 5);
+           //Expected  O/P [1,4,9,16,25]</integer>
+- [ ] Q25: Find and print all distinct odd numbers
+           List<Integer> integerList = Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9, 9, 10);
+           //Expected O/P [1,3,5,7,9]
